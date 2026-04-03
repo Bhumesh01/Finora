94f72c10bc9c4dcafc5a4edbf31a5acfe9a75169
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-04-03 23:13:44 +0530

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Card from "./components/Card"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="h-screen flex flex-col">
-      <div>NaBar</div>
+      <Navbar></Navbar>
       <div className="flex justify-between px-2 my-5 flex-wrap gap-5">
         <Card label="Total Income" color="red" amount="₹ 70000" note="Increased Last Month"></Card>
         <Card label="Total Balance" color="blue" amount="₹ 50000" note="Increased Last Month"></Card>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import ModeLogo from "../assets/mode_logo.png"
+
+export default function Navbar(){
+    return(
+        <div className="flex justify-between items-center py-4 px-6 bg-white shadow-md">
+            <h1 className="text-3xl font-bold text-gray-800 tracking-wide">Finora</h1>
+            <div className="flex items-center gap-4">
+                <select className="bg-blue-500 hover:bg-blue-600 transition-all text-white px-4 py-2 rounded-xl font-medium outline-none cursor-pointer">
+                    <option>Role: Admin</option>
+                    <option>Role: Viewer</option>
+                </select>
+                <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer transition">
+                    <img width="28" src={ModeLogo} alt="Switch Mode Logo" className="hover:scale-110 transition-transform"/>
+                </div>
+
+            </div>
+        </div>
+    )
+}
